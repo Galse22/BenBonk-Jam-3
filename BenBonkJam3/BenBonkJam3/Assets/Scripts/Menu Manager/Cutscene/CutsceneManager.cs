12f09a3452719b947caf1a6d5709521af35a356e
Galse22 <al.g.se622@gmail.com>
2021-06-26 22:20:04 -0300

    Cutscene Code

---
+++ BenBonkJam3/BenBonkJam3/Assets/Scripts/Menu Manager/Cutscene/CutsceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public Sprite[] imageArray;
+    public Image baseImage;
+    public int sceneNumber;
+    int amountsButton;
+
+    public GameObject buttonSFX;
+
+    public void ChangeImage()
+    {
+        amountsButton++;
+        GameObject goInstantiated = Instantiate(buttonSFX, Vector3.zero, Quaternion.identity);
+        //goInstantiated.GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+        if(amountsButton < imageArray.Length)
+        {
+            baseImage.overrideSprite = imageArray[amountsButton];
+        }
+        else
+        {
+            ActualLoadScene();
+        }
+    }
+
+    void ActualLoadScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
