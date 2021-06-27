97ac2a212f225a874d243be025073539f6ab0c70
Galse22 <al.g.se622@gmail.com>
2021-06-26 22:00:37 -0300

    Menu

---
+++ BenBonkJam3/BenBonkJam3/Assets/Scripts/Menu Manager/MenuManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    public GameObject buttonSFX;
+    public void TransitionToScene(int i)
+    {
+        GameObject goInstantiated = Instantiate(buttonSFX, Vector3.zero, Quaternion.identity);
+        //goInstantiated.GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+        SceneManager.LoadScene(i);
+    }
+}
